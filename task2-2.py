e9a0c7183a6cee6976451b61765811551a16f46a
Hayley <haysho2260@gmail.com>
2024-02-04 11:52:28 -0800

    add mallor attack for unsecure values of alpha

--- task2-2.py
+++ task2-2.py
@@ -27,47 +27,80 @@ def sha_hash(user_text, digest_size):
     return truncated_digest
 
 if __name__ == "__main__":
-    alpha = "A4D1CBD5 C3FD3412 6765A442 EFB99905 F8104DD2 58AC507F D6406CFF 14266D31 266FEA1E 5C41564B 777E690F 5504F213 160217B4 B01B886A 5E91547F 9E2749F4 D7FBD7D3 B9A92EE1 909D0D22 63F80A76 A6A24C08 7A091F53 1DBF0A01 69B6A28A D662A4D1 8E73AFA3 2D779D59 18D08BC8 858F4DCE F97C2A24 855E6EEB 22B3B2E5"
+    
+    # alpha = "A4D1CBD5 C3FD3412 6765A442 EFB99905 F8104DD2 58AC507F D6406CFF 14266D31 266FEA1E 5C41564B 777E690F 5504F213 160217B4 B01B886A 5E91547F 9E2749F4 D7FBD7D3 B9A92EE1 909D0D22 63F80A76 A6A24C08 7A091F53 1DBF0A01 69B6A28A D662A4D1 8E73AFA3 2D779D59 18D08BC8 858F4DCE F97C2A24 855E6EEB 22B3B2E5"
     q = "B10B8F96 A080E01D DE92DE5E AE5D54EC 52C99FBC FB06A3C6 9A6A9DCA 52D23B61 6073E286 75A23D18 9838EF1E 2EE652C0 13ECB4AE A9061123 24975C3C D49B83BF ACCBDD7D 90C4BD70 98488E9C 219A7372 4EFFD6FA E5644738 FAA31A4F F55BCCC0 A151AF5F 0DC8B4BD 45BF37DF 365C1A65 E68CFDA7 6D4DA708 DF1FB2BC 2E4A4371"
     
-    # run diffie hellman
-    xA, yA = dhke(int(alpha.replace(" ", ""), 16), int(q.replace(" ", ""), 16))
-    xB, yB = dhke(int(alpha.replace(" ", ""), 16), int(q.replace(" ", ""), 16))
-
-    # mallory attack - modify public key (YA) to YA→q
-    random_exponent = random.randint(1, int(q.replace(" ", ""), 16))
-    yA_mal = pow(yA, random_exponent, int(q.replace(" ", ""), 16))
-    yB_mal = pow(yB, random_exponent, int(q.replace(" ", ""), 16))
-
-    # A sends her encrypted message to B using A's 
-    # private key xA and Mallory's public key
-    sA = pow(yB_mal, xA, int(q.replace(" ", ""), 16))
-    sB = pow(yA_mal, xB, int(q.replace(" ", ""), 16))
-
-    # hash the mod values
-    keyA = sha_hash(str(sA), 16)
-    keyB = sha_hash(str(sB), 16)
+    alphas = [str(hex(1)[2:]), q, str(hex(int(q.replace(" ", ""), 16) - 1)[2:].upper())]
+    for alpha in alphas:
+        print("\nAlpha", alpha)
+        # run diffie hellman
+        xA, yA = dhke(int(alpha.replace(" ", ""), 16), int(q.replace(" ", ""), 16))
+        xB, yB = dhke(int(alpha.replace(" ", ""), 16), int(q.replace(" ", ""), 16))
         
-    # write message
-    mA = "This is a secret."
-    mB = "I'm trying to deliver a message."
+        # A sends her encrypted message to B using A's 
+        # private key xA and Mallory's public key
+        sA = pow(yB, xA, int(q.replace(" ", ""), 16))
+        sB = pow(yA, xB, int(q.replace(" ", ""), 16))
 
-    # generate cipher, pad text, and encrypt
-    cA = AES.new(keyA.encode('utf-8'), AES.MODE_ECB)
-    pA = pad(mA.encode('utf-8'), AES.block_size, style='pkcs7')
-    eA = cA.encrypt(pA)
-    
-    # generate cipher, pad text, and encrypt
-    cB = AES.new(keyB.encode('utf-8'), AES.MODE_ECB)
-    pB = pad(mB.encode('utf-8'), AES.block_size, style='pkcs7')
-    eB = cB.encrypt(pB)
+        # hash the mod values
+        keyA = sha_hash(str(sA), 16)
+        keyB = sha_hash(str(sB), 16)
+            
+        # write message
+        mA = "This is a secret."
+        mB = "I'm trying to deliver a message."
+
+        # generate cipher, pad text, and encrypt
+        cA = AES.new(keyA.encode('utf-8'), AES.MODE_ECB)
+        pA = pad(mA.encode('utf-8'), AES.block_size, style='pkcs7')
+        eA = cA.encrypt(pA)
         
-    # decrypt eA
-    dA = cB.decrypt(eA)
-    uA = unpad(dA, AES.block_size, style='pkcs7').decode('utf-8')
-    print(uA)
-    
-    # decrypt eB
-    dB = cA.decrypt(eB)
-    uB = unpad(dB, AES.block_size, style='pkcs7').decode('utf-8')
-    print(uB)
+        # generate cipher, pad text, and encrypt
+        cB = AES.new(keyB.encode('utf-8'), AES.MODE_ECB)
+        pB = pad(mB.encode('utf-8'), AES.block_size, style='pkcs7')
+        eB = cB.encrypt(pB)
+        
+        
+        '''Mallory Attack Below: Brute Force'''
+        # the values we know: public keys: yA, yB, alpha, q, q-1
+        
+        # attempt to find private key for A
+        exA = 1
+        sA = pow(yB, exA, int(q.replace(" ", ""), 16))
+        while sA != yA:
+            exA += 1
+            sA = pow(yB, exA, int(q.replace(" ", ""), 16)) 
+            
+        # attempt to find private key for B
+        exB = 1
+        sB = pow(yA, exB, int(q.replace(" ", ""), 16))
+        while sB != yB:
+            exB += 1
+            sB = pow(yA, exB, int(q.replace(" ", ""), 16)) 
+            
+         # hash the mod values
+        keyA = sha_hash(str(sA), 16)
+        keyB = sha_hash(str(sB), 16)
+
+        # generate cipher, pad text, and encrypt
+        cA = AES.new(keyA.encode('utf-8'), AES.MODE_ECB)
+        pA = pad(mA.encode('utf-8'), AES.block_size, style='pkcs7')
+        eA = cA.encrypt(pA)
+        
+        # generate cipher, pad text, and encrypt
+        cB = AES.new(keyB.encode('utf-8'), AES.MODE_ECB)
+        pB = pad(mB.encode('utf-8'), AES.block_size, style='pkcs7')
+        eB = cB.encrypt(pB)
+            
+        # decrypt eA
+        dA = cB.decrypt(eA)
+        uA = unpad(dA, AES.block_size, style='pkcs7').decode('utf-8')
+        print(uA)
+        
+        # decrypt eB
+        dB = cA.decrypt(eB)
+        uB = unpad(dB, AES.block_size, style='pkcs7').decode('utf-8')
+        print(uB)
+            
+            
